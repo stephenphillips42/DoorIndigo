e7e8cf903262525147e51db6cdb4e429f0dceb81
Peter Ballen <pballen@grwb-20.CIS.upenn.edu>
2014-12-03 16:12:44 -0500

    k means added

--- code/kmeansAttempt.m
+++ code/kmeansAttempt.m
@@ -1,15 +1,33 @@
 %% Kmeans
-K = 100;
-%%
-Zprime = Z;
-Z = Zprime(:,1:500);
-%% Rerun kmeans
-tic
-clusterIds = kmeans(Z,K);
-toc
-%% Run GMM
+clear;
+load ../data/city_train.mat
+load ../data/city_test.mat
+load ../data/word_train.mat
+load ../data/word_test.mat
+load ../data/bigram_train.mat
+load ../data/bigram_test.mat
+load ../data/price_train.mat
 
-% fitgmdist(X,k) Never mind this would take way too long
+X_train =[city_train word_train bigram_train];
+[N,p] = size(X_train);
+Y_train = price_train;
+X_test = [city_test word_test bigram_test];
+Y_city = cell2mat(cellfun(@(x) find(x,1,'first'), num2cell(city_train,2),'UniformOutput',false));
+
+load('pcaV.mat','V');
+Z = [word_train bigram_train] * V;
+
+K = 100; % Number of clusters
+loading = true;
+
+%% Rerun kmeans
+if loading
+    load('kmeansStuff100_small.mat')
+else
+    tic
+    clusterIds = kmeans(Z,K);
+    toc
+end
 
 %%
 clusterMeans = zeros(K,size(Z,2));
@@ -20,19 +38,10 @@ for i = 1:K
 end
 save('kmeansStuff5.mat','Z','clusterMeans','clusterIds','clusterPrices');
 
-%% Load Kmeans
-%load('kmeansStuff20.mat')
+%
+% figure;
+% plot3(clusterMeans(:,1),clusterMeans(:,2),clusterPrices,'r.');
 
-figure;
-plot3(clusterMeans(:,1),clusterMeans(:,2),clusterPrices,'r.');
-%%
-sample = unidrnd(100,length(Y_train),1);
-scatter3(Z(sample,plotpc1),...
-          Z(sample,plotpc2),...
-          Z(sample,plotpc3),...
-          (Y_train(sample)).^11/10^10,...
-          100*(Y_train(sample)),...
-          '.');
 %%
 figure; hold on
 plotpc1=1;
@@ -55,6 +64,7 @@ colorbar
 xlabel('Principal Components')
 ylabel('Price')
 hold off
+pause(3)
 
 %% Radial basis functions with the means
 % Compute the feature vectors
@@ -63,18 +73,11 @@ rbf_train = zeros(length(Y_train),K);
 for i = 1:K
     rbf_train(:,i) = exp(-sum((repmat(clusterMeans(i,:),length(Z),1)-Z).^2,2)/(2*sigma^2));
 end
-% 
-% for i = unidrnd(K,10,1)
-%     figure(3)
-%     hist(rbf_train(:,i))
-%     title(sprintf('RBF %d',i))
-%     pause(1)
-% end
-mean(std(rbf_train,[],2))
-plot(std(rbf_train,[],1))
 
 %% Create training and testing sets
+
 [trainind, testind] = crossvalind('HoldOut', length(Y_train), 0.5);
+
 X = [city_train(trainind,:) Z(trainind,:) rbf_train(trainind,:)];
 Xtest = [city_train(testind,:) Z(testind,:) rbf_train(testind,:)];
 Y = Y_train(trainind);
@@ -83,22 +86,27 @@ clusterIdsTrain = clusterIds(trainind);
 clusterIdsTest = clusterIds(testind);
 
 %% LASSO :D
-tic
-[Slope, Fitinfo] = lasso(X,Y);
-toc
-
-save('lassoWithRBF100_all.mat','Slope','Fitinfo');
-
+if loading
+    load('lassoWithRBF100_old.mat','Slope','Fitinfo');
+else
+    tic
+    [Slope, Fitinfo] = lasso(X,Y);
+    toc
+end
 
 %%
 Slope0 = Slope(:,11);
 Intercept0 = Fitinfo.Intercept(1,11);
-Yhat = Xtest*Slope0 + Intercept0;
 
-norm(Yhat-Ytest)/sqrt(length(Ytest))
-plot(Ytest, Ytest-Yhat,'r.')
-hold on
-plot(Ytest,zeros(size(Ytest)),'b.')
-hold off
+Ztest = [word_test bigram_test] * V;
+sigma = 10; % TODO: Tune this. Question: HOW? Lasso takes WAY too long.
+rbf_test = zeros(size(Ztest,1),K);
+for i = 1:K
+    rbf_test(:,i) = exp(-sum((repmat(clusterMeans(i,:),length(Ztest),1)-Ztest).^2,2)/(2*sigma^2));
+end
+Xtest = [city_test Ztest rbf_test];
+
+Yhat = Xtest*Slope0 + Intercept0;
+dlmwrite('submit.txt',Yhat,'precision','%d');
 
 

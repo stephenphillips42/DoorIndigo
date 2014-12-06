2b5e1a4f4c6e944d44c6c217fab20a07aeec7238
Stephen Phillips <stephi@grwb-19.CIS.upenn.edu>
2014-12-06 17:33:13 -0500

    Fixed things up with the interactive terms

--- code/Experimentation.m
+++ code/Experimentation.m
@@ -11,11 +11,14 @@ X_train =[city_train word_train bigram_train];
 [N,p] = size(X_train);
 Y_train = price_train;
 X_test = [city_test word_test bigram_test];
-Y_city = cell2mat(cellfun(@(x) find(x,1,'first'), num2cell(city_train,2),'UniformOutput',false));
+%Y_city = cell2mat(cellfun(@(x) find(x,1,'first'), num2cell(city_train,2),'UniformOutput',false));
 
-initialize_additional_features;
-
-
+%initialize_additional_features;
+load('pcaV500.mat','VV');
+Z = [word_train bigram_train]*VV;
+Ztest = [word_test bigram_test]*VV;
+load('wordsel.mat')
+load('bigramsel.mat')
 
 
 %% Cross validation of Experiment (template)
@@ -85,8 +88,8 @@ smallLabels(smallLabels ~= 1) = 2;
 
 smallLabels_train = smallLabels(trainind);
 smallLabels_test = smallLabels(testind);
-X = Z(trainind,1:500);
-Xtest = Z(testind,1:500);
+X = Z(trainind,:);
+Xtest = Z(testind,:);
 tic
 smallSvm = svmtrain(X,smallLabels_train,'kernel_function','rbf','rbf_sigma',10);
 toc
@@ -94,65 +97,61 @@ smallLabelHat = svmclassify(smallSvm,Xtest);
 
 fprintf('Classification error by small SVM: %f%%\n',100*sum(smallLabelHat ~= smallLabels_test)/length(smallLabels_test));
 
-%% Create training and testing sets
+% Create training and testing sets
 
-%[trainind, testind] = crossvalind('HoldOut', length(Y_train), 0.5);
-trainind = 1:length(Y_train);
+% [trainind, testind] = crossvalind('HoldOut', length(Y_train), 0.5);
+% trainind = 1:length(Y_train);
 Y = Y_train(trainind);
-% Ytest = Y_train(testind);
+Ytest = Y_train(testind);
 
 % Compute the feature vectors
 tic
-sigma = 8; % TODO: Tune this. Question: HOW? Lasso takes WAY too long.
-rbf_train = zeros(length(Y_train),K+1);
-for i = 1:K
-    rbf_train(:,i) = exp(-sum((repmat(clusterMeans(i,:),length(Z),1)-Z).^2,2)/(2*sigma^2));
-end
+% sigma = 8; % TODO: Tune this. Question: HOW? Lasso takes WAY too long.
+% rbf_train = zeros(length(Y_train),K+1);
+% for i = 1:K
+%     rbf_train(:,i) = exp(-sum((repmat(clusterMeans(i,:),length(Z),1)-Z).^2,2)/(2*sigma^2));
+% end
 
-smallLabelTrain = svmclassify(smallSvm,Z(trainind,1:500));
-% smallLabelTest = svmclassify(smallSvm,Z(testind,1:500));
-smallLabelTrain = 2*(smallLabelTrain-1.5);
-% smallLabelTest = 2*(smallLabelTest-1.5);
+smallLabelTrain = (svmclassify(smallSvm,Z(trainind,:)) == 1);
+smallLabelTest = (svmclassify(smallSvm,Z(testind,:)) == 1);
 
 npcs=500;
-interinds = 1:20;
+interinds = [ 2:21 ];
 interterms = [ (interinds)' (interinds)'; nchoosek(interinds,2) ];
 Zinter = Z(:,interterms(:,1)) .* Z(:,interterms(:,2));
-Ztrain = Z(trainind,1:npcs);
-% Ztest = Z(testind,1:npcs);
+Ztrain = Z(trainind,:);
+Ztest = Z(testind,:);
 Zintertrain = Zinter(trainind,:);
-% Zintertest = Zinter(testind,:);
+Zintertest = Zinter(testind,:);
 X = full([...
      city_train(trainind,:) ...
      Ztrain ...
      Zintertrain ...
-     rbf_train(trainind,:) ...
      word_train(trainind,wordsel(1:300)) ...
      bigram_train(trainind,bigramsel(1:200)) ...
      smallLabelTrain ]);
-% Xtest = full([...
-%          city_train(testind,:) ...
-%          Ztest ...
-%          Zintertest ...
-%          rbf_train(testind,:) ...
-%          word_train(testind,wordsel(1:300)) ...
-%          bigram_train(testind,bigramsel(1:200)) ...
-%          smallLabelTest ]);
+Xtest = full([...
+         city_train(testind,:) ...
+         Ztest ...
+         Zintertest ...
+         word_train(testind,wordsel(1:300)) ...
+         bigram_train(testind,bigramsel(1:200)) ...
+         smallLabelTest ]);
 toc
 size(X)
 
 % w = (Ztrain'*Ztrain + 1*eye(size(Ztrain,2))) \ Ztrain'*Y;
 tic
 % Next up: Lambda of 0.0001!!
-[w, Fitinfo] = lasso(X,Y,'Lambda',0.01,'Alpha',0.4);
+[w, Fitinfo] = lasso(X,Y,'Lambda',0.01,'Alpha',0.5);
 toc
 b = Fitinfo.Intercept(1);
 Yhat = (Xtest*w + b);
-% disp(norm(Ytest-Yhat)/sqrt(length(Ytest)))
-%%
-plot(Ytest,Yhat,'r.');
+disp(norm(Ytest-Yhat)/sqrt(length(Ytest)))
+
+plot(Ytest,Yhat-Ytest,'r.');
 hold on;
-plot(Ytest,Ytest,'b.');
+plot(Ytest,Ytest-Ytest,'b.');
 axis image
 
 %%
@@ -197,8 +196,17 @@ dlmwrite('submit.txt',Yhat,'precision','%d');
 
 
 %% Unsupervised Neural Net (I know...)
+clear;
+load ../data/city_train.mat
+load ../data/city_test.mat
+load ../data/word_train.mat
+load ../data/word_test.mat
+load ../data/bigram_train.mat
+load ../data/bigram_test.mat
+load ../data/price_train.mat
 
-
-
+% Principal components
+load('pcaV500','VV');
+Zall = [word_train bigram_train] * VV;
 
 
